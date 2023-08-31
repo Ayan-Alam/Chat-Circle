4c32135bb62d6cdf1d19ac5f4f07930656d95adc
Ayan-Alam <ayanalam186@gmail.com>
2023-08-31 23:36:51 +0530

    "Lets make the Chat App real time"

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,23 +1,19 @@
 const sendButton = document.getElementById("sendButton");
-document.addEventListener('DOMContentLoaded',async function(){
-    try{
-    const messageContainer = document.getElementById("messageContainer");
-    const token = localStorage.getItem("token");
-    const response = await axios.get('http://localhost:3000/chat/getMsg',{ headers : { Authorization : token }});
-    response.data.forEach((e) => {
-    const messageText = e.msg;
-      if (messageText !== "") {
-        const messageBlock = document.createElement("div");
-        messageBlock.className = "message-block other-message";
-        messageBlock.textContent = messageText;
-        messageContainer.appendChild(messageBlock);
-        messageInput.value = ""; 
-      }
-    });
-    } catch (err){
-        console.log(err);
-    }
-})
+    setInterval(async function(){
+      const messageContainer = document.getElementById("messageContainer");
+      const token = localStorage.getItem("token");
+      messageContainer.innerHTML = " ";
+      const response = await axios.get('http://localhost:3000/chat/getMsg',{ headers : { Authorization : token }});
+      response.data.forEach((e) => {
+      const messageText = e.msg;
+        if (messageText !== "") {
+          const messageBlock = document.createElement("div");
+          messageBlock.className = "message-block other-message";
+          messageBlock.textContent = messageText;
+          messageContainer.appendChild(messageBlock);
+          }
+        });
+    },1000) 
 
 document.getElementById('sendButton').addEventListener('click',async function(){
     try{
